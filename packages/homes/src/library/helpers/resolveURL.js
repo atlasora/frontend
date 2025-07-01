dc5cf765a9bf8062f4d847b5a23b51be01504a1b
Christopher McCreadie <cjmccreadie@jellyfin.local>
2025-07-01 18:39:33 +0700

    image fix

--- packages/homes/src/library/helpers/resolveURL.js
+++ packages/homes/src/library/helpers/resolveURL.js
@@ -1,6 +1,6 @@
 export default function resolveURL(url) {
   if (!url) return '';
-
+  console.log(url);
   // Check if the URL is already absolute (starts with http or https)
   if (/^https?:\/\//i.test(url)) {
     return url;
