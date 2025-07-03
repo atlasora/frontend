baf07552b0b9a4081aa555a4b82165d0cf90b671
Christopher McCreadie <cjmccreadie@jellyfin.local>
2025-07-04 01:07:49 +0700

    fixed username

--- packages/homes/src/components/UI/CommentCard/CommentCard.jsx
+++ packages/homes/src/components/UI/CommentCard/CommentCard.jsx
@@ -18,7 +18,7 @@ export default class App extends React.Component {
     const postTime = new Date(commentDate).getTime();
     const authorAvatar = singleReview?.users_permissions_user?.picture?.url
       ? resolveURL(singleReview.users_permissions_user.picture.url)
-      : resolveURL('/images/avatar.jpeg');
+      : 'https://frontend-g1i.pages.dev/images/avatar.jpeg';
     console.log(singleReview);
     // authorAvatar{resolveURL(gallery[0]?.url) || '/images/single-post-bg.jpg'}
     const reviewRating = singleReview ? singleReview.reviewFields : '';
